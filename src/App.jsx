709342c9d1c2ba35ea6872ee1b51e8ad56a20f41
Parthiv Shah <parthivjs12@gmail.com>
2025-08-04 00:18:56 -0500

    initial vite app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, Title } from '@mantine/core';
+import './App.css'
+
+function App() {
+    return (
+        <div style={{ padding: 20 }}>
+          <Title>Hello Vite + React + Mantine!</Title>
+          <Button mt="md" color="blue">Click me</Button>
+        </div>
+    )
+}
+
+export default App
